99d523bc73e0a846ab06f405ab6d40704ec95d22
faiz1701 <thefaiz17@gmail.com>
2022-11-09 23:36:18 +0700

    :art: update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.myapplication
 
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var path = Path()
+        var paintBrush = Paint()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
         val redBtn = findViewById<ImageButton>(R.id.redColor)
         val blueBtn = findViewById<ImageButton>(R.id.blueColor)
@@ -15,16 +27,16 @@ class MainActivity : AppCompatActivity() {
         val eraser = findViewById<ImageButton>(R.id.whiteColor)
 
         redBtn.setOnClickListener {
-
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
         blueBtn.setOnClickListener {
-
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
         blackBtn.setOnClickListener {
-
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
         eraser.setOnClickListener {
-
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
